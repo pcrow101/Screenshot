fec0c7ff6a0a0eb65b6a9e0619e2f61ef52ef6fa
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2025-10-26 10:49:31 +0000

    Initial Commit

---
+++ Screenshot/ScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotApp.swift
+//  Screenshot
+//
+//  Created by paul crow on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
